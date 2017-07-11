e13a8bf200b9a702c5934dfd33152b4370a9f94b
James Kominick <james.kominick@gmail.com>
2017-07-10 23:06:59 -0400

    initial

---
+++ README.md
@@ -0,0 +1,9 @@
+# shout
+
+> Work in progress server for streaming output to multiple subscribers
+
+## Setup
+
+* Requires `python3.6+`
+* `pip install -r requirements.txt`
+
---
+++ shout.py
@@ -0,0 +1,99 @@
+# usage: spin up the server
+#
+# in browser:
+# var sock = new WebSocket('ws://localhost:3000/ws');
+# sock.send(JSON.stringify({init_for: 'main'}))
+#
+# var sub1 = new WebSocket('ws://localhost:3000/ws');
+# sub1.send(JSON.stringify({subscribe_to: 'main'}))
+# sub1.onmessage = (e) => console.log('sub1-receive: ' + e.data);
+#
+# var sub2 = new WebSocket('ws://localhost:3000/ws');
+# sub2.send(JSON.stringify({subscribe_to: 'main'}))
+# sub2.onmessage = (e) => console.log('sub2-receive: ' + e.data);
+#
+# sock.send(JSON.stringify({data: 'this is data!'}))
+# ~> sub1-receive: {"data": "this is data"}
+# ~> sub2-receive: {"data": "this is data"}
+
+
+import asyncio
+import asyncpg
+from aiohttp import web, WSMsgType
+
+import json
+import logging
+
+
+access_log = logging.getLogger('aiohttp.access')
+access_log.setLevel(logging.INFO)
+ch = logging.StreamHandler()
+access_log.addHandler(ch)
+
+socket_log = logging.getLogger(__name__ + '_socket_log')
+socket_log.setLevel(logging.INFO)
+ch = logging.StreamHandler()
+socket_log.addHandler(ch)
+
+
+async def handle(request):
+    pool = request.app['pool']
+    power = int(request.match_info.get('power', 10))
+    async with pool.acquire() as conn:
+        async with conn.transaction():
+            result = await conn.fetchval('select 2 ^ $1', power)
+            resp = f'2 ^ {power} is {result}'
+            return web.Response(text=resp)
+
+
+async def sock(request):
+    ws = web.WebSocketResponse()
+    await ws.prepare(request)
+    async for msg in ws:
+        if msg.type == WSMsgType.CLOSED:
+            await ws.close()
+        if msg.type == WSMsgType.TEXT:
+            data = json.loads(msg.data)
+            sock_id = data.get('init_for')
+            if sock_id:
+                request.app['ws_to_id'][ws] = sock_id
+                request.app['id_to_ws'][sock_id] = ws
+                request.app['sockets'][sock_id] = {'subscribers': []}
+                socket_log.info(f'initialized source: {sock_id}')
+                continue
+
+            sock_id = data.get('subscribe_to')
+            if sock_id:
+                request.app['sockets'][sock_id]['subscribers'].append(ws)
+                socket_log.info(f'subscribed to: {sock_id}')
+                continue
+
+            sock_id = request.app['ws_to_id'][ws]
+            subscribers = request.app['sockets'][sock_id]['subscribers']
+            for sub in subscribers:
+                await sub.send_json({'data': data['data']})
+
+        elif msg.type == WSMsgType.ERROR:
+            socket_log.error(f'ws connection closed with error: {ws.exception()}')
+    socket_log.info('websocket closed')
+    return ws
+
+
+async def init_app():
+    app = web.Application()
+
+    app['pool'] = await asyncpg.create_pool(database='shout', user='shout', password='shout', host='localhost')
+    app.router.add_route('GET', '/{power:\d+}', handle)
+    app.router.add_route('GET', '/', handle)
+    app.router.add_route('GET', '/ws', sock)
+    app.make_handler(access_log=access_log)
+    app['sockets'] = {}
+    app['ws_to_id'] = {}
+    app['id_to_ws'] = {}
+    return app
+
+
+loop = asyncio.get_event_loop()
+app = init_app()
+app = loop.run_until_complete(app)
+web.run_app(app, port=3000)
